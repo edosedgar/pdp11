5724be23c984a8924bd7c1e190e1101ff2fc0921
Edgar Kaziakhmedov <edgar.kaziakhmedov@virtuozzo.com>
2018-01-21 15:58:16 +0300

    gui: improve
    Signed-off-by: Edgar Kaziakhmedov <edgar.kaziakhmedov@virtuozzo.com>

--- gui.py
+++ gui.py
@@ -4,24 +4,77 @@ import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('PangoCairo', '1.0')
 from gi.repository import Gtk, Gdk, Pango, GObject, cairo, PangoCairo
+import socket, sys
 
-"""class SearchDialog(Gtk.Dialog):
+class SourceCode():
 
-    def __init__(self, parent):
-        Gtk.Dialog.__init__(self, "Search", parent,
-            Gtk.DialogFlags.MODAL, buttons=(
-            Gtk.STOCK_FIND, Gtk.ResponseType.OK,
-            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL))
+    def __init__(self, textview, server):
+        self.code = dict(((i, ["HALT", 2]) for i in range (0, 0xE000)))
+        self.textview = textview
+        self.server = server
 
-        box = self.get_content_area()
+    def fetch_code(self, start, end):
+        count = start
+        while (count < end):
+            command, size = self.server.em_recv_command(count)
+            self.code[count] = command, size
+            count += size
 
-        label = Gtk.Label("Insert text you want to search for:")
-        box.add(label)
+    def show_rom(self):
+        count = 0
+        string = ""
+        while (count < 0xE000):
+            command, size = self.code[count]
+            string += "{0:#0{1}x}".format(count, 6)
+            string += "        "
+            string += command.replace("\n", "") + "\n"
+            count += size
+        textbuffer = self.textview.get_buffer()
+        textbuffer.set_text(string)
 
-        self.entry = Gtk.Entry()
-        box.add(self.entry)
+    def show_cur_line(self, address):
 
-        self.show_all()"""
+class Server():
+    def __init__(self):
+        self.TCP_IP = '127.0.0.1'
+        self.TCP_PORT = 6700
+        BUFFER_SIZE = 1024
+
+        self.channel = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.channel.connect((self.TCP_IP, self.TCP_PORT))
+    def em_recv_reply(self):
+        reply = self.channel.recv(256)
+        if (reply[0] == '0'):
+            return "ok"
+        else:
+            return "err"
+
+    def em_send_init_gui(self):
+        self.channel.send("em_init_gui")
+        return self.em_recv_reply()
+
+    def em_send_load_file(self, file_path):
+        self.channel.send("em_load_file")
+        reply = self.em_recv_reply()
+        if reply == "ok":
+            self.channel.send(file_path)
+        else:
+            return "err"
+        return self.em_recv_reply()
+
+    def em_recv_command(self, address):
+        self.channel.send("em_get_command")
+        self.channel.send(str(address))
+        command = self.channel.recv(256)
+        size = int(self.channel.recv(256))
+        self.channel.send("ok")
+        return command, size
+
+    def receive(self):
+        data = channel.recv(message)
+
+    def close(self):
+        self.channel.close()
 
 class Emulator(Gtk.Window):
 
@@ -29,6 +82,7 @@ class Emulator(Gtk.Window):
         Gtk.Window.__init__(self, title="PDP11 Emulator")
 
         self.set_default_size(1024, 768)
+        self.set_resizable(False)
 
         self.grid = Gtk.Grid()
         self.add(self.grid)
@@ -36,7 +90,19 @@ class Emulator(Gtk.Window):
         self.create_toolbar()
         self.create_sourceviewer()
         self.create_display()
+        self.create_stateviewer()
         self.breakpoint_list = [];
+
+        self.server = Server()
+        ret = self.server.em_send_init_gui()
+        if (ret == "err"):
+            dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.ERROR,
+                                       Gtk.ButtonsType.OK,
+                                       "Client refused connection")
+            dialog.run()
+            dialog.destroy()
+            sys.exit(1)
+        self.curr_address = 0x0000
 
     def create_toolbar(self):
         toolbar = Gtk.Toolbar()
@@ -74,6 +140,7 @@ class Emulator(Gtk.Window):
         button_next.set_label("Step");
         button_next.set_is_important(True)
         toolbar.insert(button_next, 5)
+        button_next.connect("clicked", self.button_next_clicked)
 
         button_toggle_b = Gtk.ToolButton()
         button_toggle_b.set_icon_name("media-record")
@@ -91,32 +158,45 @@ class Emulator(Gtk.Window):
         self.scrolledwindow.set_vexpand(True)
         self.scrolledwindow.set_shadow_type(Gtk.ShadowType.IN)
         self.grid.attach_next_to(self.scrolledwindow, self.toolbar,
-                                 Gtk.PositionType.BOTTOM, 1, 1)
+                                 Gtk.PositionType.BOTTOM, 1, 2)
 
         self.textview = Gtk.TextView()
         self.textbuffer = self.textview.get_buffer()
-        self.textbuffer.set_text("0x0000 MOV R1, R2 \n"
-                                 "0x0002 HALT");
         self.textview.set_editable(False)
-        self.textview.modify_font(Pango.FontDescription('Monospace 20'))# OK
+        self.textview.modify_font(Pango.FontDescription('Monospace 19'))
+        self.textview.set_cursor_visible(False)
 
         self.scrolledwindow.add(self.textview)
 
-        self.tag_bold = self.textbuffer.create_tag("bold",
-            weight=Pango.Weight.BOLD)
-        self.tag_italic = self.textbuffer.create_tag("italic",
-            style=Pango.Style.ITALIC)
-        self.tag_underline = self.textbuffer.create_tag("underline",
-            underline=Pango.Underline.SINGLE)
         self.tag_breakpoint = self.textbuffer.create_tag("breakpoint",
             background="red")
+
+    def create_stateviewer(self):
+        viewer = Gtk.TextView()
+        self.grid.attach(viewer, 1, 1, 2, 1)
+
+        self.stateview = Gtk.TextView()
+        self.statebuffer2 = self.stateview.get_buffer()
+        self.statebuffer2.set_text("\n  |> R0 = 0x0000 <|> R1 = 0x0000 <|\n"
+                                   "  |> R2 = 0x0000 <|> R3 = 0x0000 <|\n"
+                                   "  |> R4 = 0x0000 <|> R5 = 0x0000 <|\n"
+                                   "  |> R6 = 0x0000 <|> R7 = 0x0000 <|\n"
+                                   "  |>    C = 0 | V = 0 | Z = 0    <|\n"
+                                   "  |>    N = 0 | T = 0 | I = 0    <|\n"
+                                   "  |>  Time  = 0000000000000 mcs  <|\n"
+                                   "  |>  Cycle = 0000000000000 cyc  <|\n")
+        self.stateview.set_editable(True)
+        self.stateview.modify_font(Pango.FontDescription('Monospace 18'))# OK
+        self.stateview.set_cursor_visible(False)
+
+        self.grid.attach_next_to(self.stateview, viewer, Gtk.PositionType.BOTTOM, 2, 1)
 
     def create_display(self):
         self.display = Gtk.DrawingArea();
         self.display.set_size_request(512, 512);
         self.grid.attach(self.display, 1, 1, 2, 1);
+        self.display.connect("draw", self.display_render);
 
-        self.display.connect('draw', self.display_render)
         self.invert = 0
         GObject.timeout_add(16, self.on_timer);
 
@@ -140,8 +220,6 @@ class Emulator(Gtk.Window):
 
         self.invert = self.invert + 1
 
-        #cr.select_font_face("Courier", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
-
     def load_button_clicked(self, widget):
         fc = Gtk.FileChooserDialog("Open.." , self, Gtk.FileChooserAction.OPEN,
                                    (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
@@ -151,6 +229,17 @@ class Emulator(Gtk.Window):
         if response == Gtk.ResponseType.OK:
             self.binary_full_name = fc.get_filename()
         fc.destroy()
+
+        self.server.em_send_load_file(self.binary_full_name)
+        self.disasm = SourceCode(self.textview, self.server)
+        self.disasm.fetch_code(0x0000, 0x0004)
+        self.current_addr = 0
+        self.disasm.show_rom()
+        self.disasm.show_cur_line(self.current_addr)
+
+    def button_next_clicked(self, widget):
+        self.current_addr += 2
+        self.disasm.show_cur_line(self.current_addr)
 
     def toggle_b_clicked(self, widget):
         try:
@@ -170,84 +259,6 @@ class Emulator(Gtk.Window):
         self.breakpoint_list.append([start, end]);
         self.textbuffer.apply_tag(self.tag_breakpoint, start, end)
 
-    """def create_buttons(self):
-        check_editable = Gtk.CheckButton("Editable")
-        check_editable.set_active(True)
-        check_editable.connect("toggled", self.on_editable_toggled)
-        self.grid.attach(check_editable, 0, 2, 1, 1)
-
-        check_cursor = Gtk.CheckButton("Cursor Visible")
-        check_cursor.set_active(True)
-        check_editable.connect("toggled", self.on_cursor_toggled)
-        self.grid.attach_next_to(check_cursor, check_editable,
-            Gtk.PositionType.RIGHT, 1, 1)
-
-        radio_wrapnone = Gtk.RadioButton.new_with_label_from_widget(None,
-            "No Wrapping")
-        self.grid.attach(radio_wrapnone, 0, 3, 1, 1)
-
-        radio_wrapchar = Gtk.RadioButton.new_with_label_from_widget(
-            radio_wrapnone, "Character Wrapping")
-        self.grid.attach_next_to(radio_wrapchar, radio_wrapnone,
-            Gtk.PositionType.RIGHT, 1, 1)
-
-        radio_wrapword = Gtk.RadioButton.new_with_label_from_widget(
-            radio_wrapnone, "Word Wrapping")
-        self.grid.attach_next_to(radio_wrapword, radio_wrapchar,
-            Gtk.PositionType.RIGHT, 1, 1)
-
-        radio_wrapnone.connect("toggled", self.on_wrap_toggled,
-            Gtk.WrapMode.NONE)
-        radio_wrapchar.connect("toggled", self.on_wrap_toggled,
-            Gtk.WrapMode.CHAR)
-        radio_wrapword.connect("toggled", self.on_wrap_toggled,
-            Gtk.WrapMode.WORD)"""
-"""
-    def on_button_clicked(self, widget, tag):
-        bounds = self.textbuffer.get_selection_bounds()
-        if len(bounds) != 0:
-            start, end = bounds
-            self.textbuffer.apply_tag(tag, start, end)
-
-    def on_clear_clicked(self, widget):
-        start = self.textbuffer.get_start_iter()
-        end = self.textbuffer.get_end_iter()
-        self.textbuffer.remove_all_tags(start, end)
-
-    def on_editable_toggled(self, widget):
-        self.textview.set_editable(widget.get_active())
-
-    def on_cursor_toggled(self, widget):
-        self.textview.set_cursor_visible(widget.get_active())
-
-    def on_wrap_toggled(self, widget, mode):
-        self.textview.set_wrap_mode(mode)
-
-    def on_justify_toggled(self, widget, justification):
-        self.textview.set_justification(justification)
-
-    def on_search_clicked(self, widget):
-        dialog = SearchDialog(self)
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            cursor_mark = self.textbuffer.get_insert()
-            start = self.textbuffer.get_iter_at_mark(cursor_mark)
-            if start.get_offset() == self.textbuffer.get_char_count():
-                start = self.textbuffer.get_start_iter()
-
-            self.search_and_mark(dialog.entry.get_text(), start)
-
-        dialog.destroy()
-
-    def search_and_mark(self, text, start):
-        end = self.textbuffer.get_end_iter()
-        match = start.forward_search(text, 0, end)
-
-        if match != None:
-            match_start, match_end = match
-            self.textbuffer.apply_tag(self.tag_found, match_start, match_end)
-            self.search_and_mark(text, match_end)
-"""
 # Main interaction
 pdp_emul = Emulator()
 pdp_emul.connect("delete-event", Gtk.main_quit)
